1fcad5df21f6ff04e108eda88058d702864ed043
Nitesh <niteshkesharwani728@gmail.com>
2024-04-08 12:27:46 +0530

    Added authorization configuration

--- src/main/java/com/spring/security/SpringSecurity/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/security/SpringSecurity/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.spring.security.SpringSecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+    //authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -20,6 +23,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("foo")
                 .password("foo")
                 .roles("ADMIN");
+    }
+
+    //authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("ADMIN")
+                .and().formLogin();
     }
 
     @Bean
