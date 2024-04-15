d5b84a15cb2437af675d1f47c8fa7e429882b501
Nitesh <niteshkesharwani728@gmail.com>
2024-04-15 13:32:17 +0530

    JDBC auth with specific users

--- src/main/java/com/spring/security/SpringSecurity/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/security/SpringSecurity/configuration/SecurityConfiguration.java
@@ -1,36 +1,61 @@
 package com.spring.security.SpringSecurity.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    DataSource dataSource;
+
     //authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+//        auth.inMemoryAuthentication()
+//                .withUser("blah")
+//                .password("blah")
+//                .roles("USER")
+//                .and()
+//                .withUser("foo")
+//                .password("foo")
+//                .roles("ADMIN");
+
+        //JDBC auth for 2 users
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("pass")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
+
+
     }
 
     //authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**")
-                .hasRole("ADMIN")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
                 .and().formLogin();
     }
 
